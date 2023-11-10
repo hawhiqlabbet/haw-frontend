20f07682e9763ba26102a95efc8e9e6679dcca10
David Ångström <david.angstrom@hiq.se>
2023-11-10 09:45:59 +0100

    Fixed dev and prod

--- angular-client/src/app/socket.service.ts
+++ angular-client/src/app/socket.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
-  private apiUrl = 'http://20.166.165.244:3001';
+  private apiUrl = environment.apiUrl
 
   constructor(public socket: Socket, private http: HttpClient) {
   }

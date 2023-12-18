a1fdc4cccffbeffe6e00ee1351d8752e875a41b9
David Ångström <david.angstrom@hiq.se>
2023-12-18 14:01:45 +0100

    Fixed algorithm for player prompt mapping

--- spring-boot/src/main/java/com/example/springboot/models/HiQlashData.java
+++ spring-boot/src/main/java/com/example/springboot/models/HiQlashData.java
@@ -4,11 +4,14 @@ import lombok.Data;
 
 import java.util.*;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 @Data
 public class HiQlashData implements LobbyData{
     int numPlayers;
     int numRounds;
-    long endTime;
+    long endTime;       //TODO: Set Time things
     long endVoteTime;
     List<PlayerPrompts> promptsForPlayers;
     List<String> prompts = Arrays.asList(
@@ -39,9 +42,13 @@ public class HiQlashData implements LobbyData{
     }
     public HiQlashData(int numPlayers, List<Player> players) {
         Collections.shuffle(this.prompts);
+        Collections.shuffle(players);
 
         this.numPlayers = numPlayers;
         this.numRounds  = numPlayers;
+
+        this.promptsForPlayers = new ArrayList<PlayerPrompts>();
+        this.playerScores = new ArrayList<PlayerScores>();
 
         for(int i = 0; i < numPlayers; ++i) {
             String playerName = players.get(i).getUsername();
@@ -51,8 +58,11 @@ public class HiQlashData implements LobbyData{
             this.playerScores.add(new PlayerScores(playerName, 0));
 
             // Determine prompts for each player
-            for(int j = 0; j < numRounds; ++j) {
-                int promptIndex = (i + j) % numPlayers;
+            for(int j = 0; j < 2; ++j) {
+                int promptIndex = i + j;
+                if(i == (numPlayers - 1) && j == 1) {
+                    promptIndex = 0;
+                }
                 playerPrompts.prompts.add(prompts.get(promptIndex));
             }
 
